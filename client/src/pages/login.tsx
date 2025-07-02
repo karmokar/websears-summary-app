bc71ac7a219936a0c299001ed6d7a1ab1a7d5fce
pranay <karmokarpranay793@gmail.com>
2025-07-02 23:06:08 +0530

    add design to login page

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/components/login-form"; //Importing Files from Comp
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 ">
-      <div className="w-full max-w-sm md:max-w-2xl"> 
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10  ">
+      <div className="w-full max-w-sm md:max-w-1xl"> 
         <LoginForm />
       </div>
     </div>
